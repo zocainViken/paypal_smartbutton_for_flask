eea546e63fa0f301a310b244f0d9cc143a4b48f9
zocainViken <75504411+zocainViken@users.noreply.github.com>
2021-12-02 08:11:26 +0100

    pylint
    first try with pylint

--- paypal/process.py
+++ paypal/process.py
@@ -1,10 +1,11 @@
-from .models import OderProcessing
 from paypal import db
+from .models import OderProcessing
+
 
 def tax_calculator(tax, price):
     # calculate %
-    # (2.9*150/100)  
-    #  ==> 
+    # (2.9*150/100)
+    #  ==>
     # tax x price / 100
     money = float(price)
     taxing = float(tax)# 2.9%
@@ -14,56 +15,94 @@ def tax_calculator(tax, price):
 
 def tax_reductor(tax, price):
     # https://www.capte-les-maths.com/pourcentage/les_pourcentages_p10.php
-    
+
     ##       Coefficient Multiplicateur = 1 +  20   = 1,2
     ##                                        _____
     ##                                         100
     #____________________________________________________________
     coeff_x = 1 + (tax / 100)#1.2
-    
-    
+
+
     ##        Prix Initial = 180  = 150
     ##                      ______
     ##                        1,2
     #____________________________________________________________
     no_tax = price / coeff_x#  150
     tax_amount = price - no_tax  #30
-    
+
     return tax_amount, no_tax
 
-
-class shippingParser():
+class ShippingParser():
     def __init__(self, data, debug=False):
         self.data = data
 
         # data send by js origin from smartbutton page
         # here I will filter the dict reponse for put the important data into database
 
-        self.order_id = data['id']#5N575537KH865605U  ==> use it in url for more detail on paypal
-        self.order_intent = data['intent']#CAPTURE
-        self.order_status = data['status']#COMPLETED
-        self.create_time = data['create_time']#2021-11-28T04:06:42Z
-        self.update_time = data['update_time']#2021-11-28T04:07:13Z
-        self.merchand_id = data['purchase_units'][0]['payee']['merchant_id']#E7VZ3S7TB4HUA
-        self.unit_reference_id = data['purchase_units'][0]['reference_id']#default
-        
-        self.street = data['purchase_units'][0]['shipping']['address']['address_line_1']#Av. de la Pelouse
-        self.city_1 = data['purchase_units'][0]['shipping']['address']['admin_area_2']#Paris
-        self.city_2 = data['purchase_units'][0]['shipping']['address']['admin_area_1']#Alsace
-        self.postal_code =data['purchase_units'][0]['shipping']['address']['postal_code']#75002
-        self.country_code = data['purchase_units'][0]['shipping']['address']['country_code']#FR
-        self.full_name =data['purchase_units'][0]['shipping']['name']['full_name']#John Doe
-        self.unit_currency_code = data['purchase_units'][0]['amount']['currency_code']#EUR
-        self.unit_amount = data['purchase_units'][0]['amount']['value']#35.00
+        self.order_id = data['id']
+        # 5N575537KH865605U  ==> use it in url for more detail on paypal
 
-        self.payer_first_name = data['payer']['name']['given_name']#John
-        self.payer_name = data['payer']['name']['surname']#Doe
-        self.payer_mail = data['payer']['email_address']#sb-s60z28668822@personal.example.com
-        self.payer_id = data['payer']['payer_id']#HPG6DCCZLRKW8
-        
-        self.link = data['links'][0]['href']#https://api.sandbox.paypal.com/v2/checkout/orders/5N575537KH865605U
-        self.rel = data['links'][0]['rel']#self
-        self.method = data['links'][0]['method']#GET
+        self.order_intent = data['intent']
+        # CAPTURE
+
+        self.order_status = data['status']
+        # COMPLETED
+
+        self.create_time = data['create_time']
+        # 2021-11-28T04:06:42Z
+
+        self.update_time = data['update_time']
+        # 2021-11-28T04:07:13Z
+
+        self.merchand_id = data['purchase_units'][0]['payee']['merchant_id']
+        # E7VZ3S7TB4HUA
+
+        self.unit_reference_id = data['purchase_units'][0]['reference_id']
+        # default
+
+        self.street = data['purchase_units'][0]['shipping']['address']['address_line_1']
+        # Av. de la Pelouse
+
+        self.city_1 = data['purchase_units'][0]['shipping']['address']['admin_area_2']
+        # Paris
+
+        self.city_2 = data['purchase_units'][0]['shipping']['address']['admin_area_1']
+        # Alsace
+
+        self.postal_code =data['purchase_units'][0]['shipping']['address']['postal_code']
+        # 75002
+
+        self.country_code = data['purchase_units'][0]['shipping']['address']['country_code']
+        # FR
+        self.full_name =data['purchase_units'][0]['shipping']['name']['full_name']
+        # John Doe
+
+        self.unit_currency_code = data['purchase_units'][0]['amount']['currency_code']
+        # EUR
+
+        self.unit_amount = data['purchase_units'][0]['amount']['value']
+        # 35.00
+
+        self.payer_first_name = data['payer']['name']['given_name']
+        # John
+
+        self.payer_name = data['payer']['name']['surname']
+        # Doe
+
+        self.payer_mail = data['payer']['email_address']
+        # sb-s60z28668822@personal.example.com
+
+        self.payer_id = data['payer']['payer_id']
+        # HPG6DCCZLRKW8
+
+        self.link = data['links'][0]['href']
+        # https://api.sandbox.paypal.com/v2/checkout/orders/5N575537KH865605U
+
+        self.rel = data['links'][0]['rel']
+        # self
+
+        self.method = data['links'][0]['method']
+        # GET
 
         # calculate paypal fee
         # (2.9*150/100)  ==> (tax x price / 100) + transaction_price
@@ -72,21 +111,28 @@ class shippingParser():
         paypal_transaction_price = 0.33# 0.33 --> EUR
         tva = 20
         self.tva_amount, _ = tax_reductor(tva, price)# 30
-        tax_amount, total = tax_calculator(tax_paypal, price)#  paypal
-        
+        tax_amount, _ = tax_calculator(tax_paypal, price)#  paypal
+
         self.for_paypal = tax_amount + paypal_transaction_price#to paypal 5.22 + 0.33 = 5.55
         self.for_me = price - self.tva_amount - self.for_paypal# 180 - 30 - 5.55 = 144.5
 
         if debug:
             print('\n\n\t\t DEBUG MODE:\n\n')
             print('\n\n\t\t', self.data, ':\n\n\n\n')
-            # customer data
-            print(f'NAME:\t{self.payer_name}\t FIRSTNAME:\t{self.payer_first_name }\n FULLNAME:\t{self.full_name}\nPAYER_ID:\t{self.payer_id } \tMAIL:\t{self.payer_mail } ')
-            print(f'\nORDER_ID:\t{self.order_id}\t ORDER_INTENT:\t{self.order_intent }\n ORDER_STATUS:\t{self.order_status}\n CREATE_TIME:\t{self.create_time } \tLAST_UPDATE:\t{self.update_time } ')
-            
-            print(f'MERCHANT_ID:\t{self.merchand_id}\t UNIT_CUR:\t{self.unit_currency_code }\nUNIT_AMOUNT:\t{self.unit_amount}\n')
-            print(f'\nSTREET:\t{self.street}\nCITY_1:\t{self.city_1 }\nCITY_2:\t{self.city_2}\nPOSTAL:\t{self.postal_code } \tCOUNTRY:\t{self.country_code } ')
-            
+            # Customer data
+            print(f'NAME:\t{self.payer_name}\t FIRSTNAME:\t{self.payer_first_name }')
+            print(f'FULLNAME:\t{self.full_name}\t PAYER_ID:\t{self.payer_id }')
+            print(f'MAIL:\t{self.payer_mail } ')
+            # Order part
+            print(f'\nORDER_ID:\t{self.order_id}\t ORDER_INTENT:\t{self.order_intent }')
+            print(f'ORDER_STATUS:\t{self.order_status}\n CREATE_TIME:\t{self.create_time }')
+            print(f'LAST_UPDATE:\t{self.update_time }')
+            print(f'MERCHANT_ID:\t{self.merchand_id}')
+            print(f'UNIT_CUR:\t{self.unit_currency_code }\t UNIT_AMOUNT:\t{self.unit_amount}')
+            # Shipping part
+            print(f'STREET:\t{self.street}\nCITY_1:\t{self.city_1 }\nCITY_2:\t{self.city_2}')
+            print(f'POSTAL:\t{self.postal_code } \tCOUNTRY:\t{self.country_code }')
+
             print('LINK:\t', self.link)
             print('REL:\t', self.rel)
             print('METHOD:\t', self.method)
@@ -95,25 +141,27 @@ class shippingParser():
             print('4 PAYPAL:\t', self.for_paypal)
             print('4 ME:\t', self.for_me)
 
-
-
+    def put_in_db(self):
         
-
-    def put_it_in_db(self):
-        print('here the function for put my data into DB')
         processing = OderProcessing(
-            order_id=self.order_id, order_intent=self.order_intent, order_status=self.order_status,
-            street=self.street, city_1=self.city_1, city_2=self.city_2, postal_code=self.postal_code,
-            country_code=self.country_code, unit_reference_id=self.unit_reference_id, unit_currency_code=self.unit_currency_code,
-            unit_amount=self.unit_amount, full_name=self.full_name, payer_first_name=self.payer_first_name,
-            payer_name=self.payer_name, payer_email=self.payer_mail, payer_id=self.payer_id, merchand_id=self.merchand_id,
-            links=self.link, rel=self.rel, method=self.method,  tva_amount=self.tva_amount, for_paypal=self.for_paypal, 
+            order_id=self.order_id, order_intent=self.order_intent,
+            order_status=self.order_status,
+            street=self.street, city_1=self.city_1, city_2=self.city_2,
+            postal_code=self.postal_code, country_code=self.country_code,
+            unit_reference_id=self.unit_reference_id, unit_currency_code=self.unit_currency_code,
+            unit_amount=self.unit_amount, full_name=self.full_name,
+            payer_first_name=self.payer_first_name,
+            payer_name=self.payer_name, payer_email=self.payer_mail,
+            payer_id=self.payer_id, merchand_id=self.merchand_id,
+            links=self.link, rel=self.rel, method=self.method,
+            tva_amount=self.tva_amount, for_paypal=self.for_paypal,
             for_me=self.for_me, create_time=self.create_time, update_time=self.update_time,
             )
-        
+
         db.session.add(processing)
         db.session.commit()
-        print('data correctly processed')
+        print('shippingParser: data correctly processed')
+
 
 
 '''if __name__ == '__main__':
@@ -127,28 +175,28 @@ response items
 
 
 ('id', '5N575537KH865605U')
-('intent', 'CAPTURE')      
+('intent', 'CAPTURE')
 ('status', 'COMPLETED')
 
 
 ('purchase_units', [{
-        'reference_id': 'default', 
+        'reference_id': 'default',
 
         'amount': {'currency_code': 'EUR',
                    'value': '35.00'
                    },
 
         'payee': {
-                   'email_address': 'sb-qrq0i8668082@business.example.com', 
+                   'email_address': 'sb-qrq0i8668082@business.example.com',
                    'merchant_id': 'E7VZ3S7TB4HUA'
-                 }, 
+                 },
 
-        'shipping': {'name': {'full_name': 'John Doe'}, 
+        'shipping': {'name': {'full_name': 'John Doe'},
                     'address': {
-                                'address_line_1': 'Av. de la Pelouse', 
-                                'admin_area_2': 'Paris', 
-                                'admin_area_1': 'Alsace', 
-                                'postal_code': '75002', 
+                                'address_line_1': 'Av. de la Pelouse',
+                                'admin_area_2': 'Paris',
+                                'admin_area_1': 'Alsace',
+                                'postal_code': '75002',
                                 'country_code': 'FR'
                                 }
                     },
@@ -156,27 +204,33 @@ response items
                     'captures': [{
                         'id': '0RU30241TS283821M',
                         'status': 'COMPLETED',
-                        'amount': {'currency_code': 'EUR', 'value': '35.00'}, 
-                        'final_capture': True, 
+                        'amount': {'currency_code': 'EUR', 'value': '35.00'},
+                        'final_capture': True,
                         'seller_protection': {
-                            'status': 'ELIGIBLE', 
-                            'dispute_categories': ['ITEM_NOT_RECEIVED', 'UNAUTHORIZED_TRANSACTION']}, 
-                            'create_time': '2021-11-28T04:07:13Z', 
+                            'status': 'ELIGIBLE',
+                            'dispute_categories': [
+                                                    'ITEM_NOT_RECEIVED',
+                                                    'UNAUTHORIZED_TRANSACTION'
+                                                    ]},
+                            'create_time': '2021-11-28T04:07:13Z',
                             'update_time': '2021-11-28T04:07:13Z'}]}}])
 
-('payer', 
+('payer',
     {'name': {
-                'given_name': 'John', 
+                'given_name': 'John',
                 'surname': 'Doe'
-            }, 
-    'email_address': 'sb-s60z28668822@personal.example.com', 
-    'payer_id': 'HPG6DCCZLRKW8', 
+            },
+    'email_address': 'sb-s60z28668822@personal.example.com',
+    'payer_id': 'HPG6DCCZLRKW8',
     'address': {'country_code': 'FR'}})
 
 
 ('create_time', '2021-11-28T04:06:42Z')
 ('update_time', '2021-11-28T04:07:13Z')
-('links', [{'href': 'https://api.sandbox.paypal.com/v2/checkout/orders/5N575537KH865605U', 'rel': 'self', 'method': 'GET'}])
+('links', [
+    {'href': 'https://api.sandbox.paypal.com/v2/checkout/orders/5N575537KH865605U',
+    'rel': 'self', 'method': 'GET'}
+    ])
 
 
 
--- paypal/routes.py
+++ paypal/routes.py
@@ -1,6 +1,6 @@
 from paypal import app
 from flask import render_template, request
-
+from .process import ShippingParser
 
 
 
@@ -10,14 +10,13 @@ def smartbutton():
 		return render_template("button/smartbutton.html")
 	except Exception as e:
 		return(str(e))
-from .process import shippingParser
 
 @app.route('/smart_button_ipn', methods=['POST'])
 def smartbuttonipn():
     # from here i receive a response from paypal after the payment was done
     # also I need to store this data into db 
     data = request.get_json()
-    shippingParser(data).put_it_in_db(True)
+    ShippingParser(data).put_it_in_db(True)
 
     return(data)
 
